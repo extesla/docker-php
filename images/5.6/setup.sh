90f217d1832bcee67d4fac253201cf1e25031cc6
Sean Quinn <swquinn@gmail.com>
2019-12-02 20:34:02 -0500

    Update PHP 5.6

--- images/5.6/setup.sh
+++ images/5.6/setup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# Copyright (c) 2015, Extesla LLC.
-#
-# It is illegal to use, reproduce or distribute any part of this
-# Intellectual Property without prior written authorization from
-# Extesla LLC.
 set -e
 
 #: ==========================================================================
